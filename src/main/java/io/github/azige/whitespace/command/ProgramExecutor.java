51247ba43c44ade30ddb27a124771cc23f102dd3
azige <zzb12@qq.com>
2014-07-21 22:27:52 +0800

    Edit executor

--- src/main/java/io/github/azige/whitespace/command/ProgramExecutor.java
+++ src/main/java/io/github/azige/whitespace/command/ProgramExecutor.java
@@ -16,24 +16,57 @@
 package io.github.azige.whitespace.command;
 
 /**
- * 用于执行Whitespace程序的执行器
+ * 用于执行Whitespace程序的执行器。执行器有一个从0开始的指令指针，反复执行程序中的指令并移动指针，直到程序末尾。
  *
  * @author Azige
  */
 public interface ProgramExecutor{
 
+    /**
+     * 执行单条语句，并将指令指针向后移动
+     *
+     * @return 如果指令指针移动，则为true，如果已经到达程序末尾则为false
+     */
     boolean executeOne();
 
+    /**
+     * 获得当前指令指针所标识的指令
+     *
+     * @return 当前指令
+     */
     Command getCurrentCommand();
 
+    /**
+     * 持续执行指令直到程序末尾
+     *
+     * @param fromStart 若为true，则在开始执行前先重置指令指针
+     */
     void executeAll(boolean fromStart);
 
+    /**
+     * 获得当前指令指针的位置
+     *
+     * @return 指令指针的位置
+     */
     int getLocation();
 
+    /**
+     * 获得此执行器正在执行的程序
+     *
+     * @return
+     */
     Program getProgram();
 
+    /**
+     * 跳转到指定的标签
+     *
+     * @param label
+     */
     void jump(String label);
 
+    /**
+     * 重置指令指针到程序开始处
+     */
     void reset();
 
 }
---
+++ src/main/java/io/github/azige/whitespace/command/SimpleProgramExecutor.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2014 Azige.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.azige.whitespace.command;
+
+import io.github.azige.whitespace.WhitespaceException;
+import io.github.azige.whitespace.vm.WhitespaceVM;
+
+/**
+ * Whitespace程序执行器。此对象在生命周期内只能用于执行一个程序，可以执行多次。
+ *
+ * @author Azige
+ */
+public class SimpleProgramExecutor implements ProgramExecutor{
+
+    private final WhitespaceVM vm;
+    private final Program program;
+    private int location = 0;
+
+    public SimpleProgramExecutor(WhitespaceVM vm, Program program){
+        this.vm = vm;
+        this.program = program;
+    }
+
+    @Override
+    public Program getProgram(){
+        return program;
+    }
+
+    @Override
+    public int getLocation(){
+        return location;
+    }
+
+    @Override
+    public Command getCurrentCommand(){
+        return program.getCommands().get(location);
+    }
+
+    @Override
+    public void jump(String label){
+        if (!program.getLabelMap().containsKey(label)){
+            throw new WhitespaceException("标签" + label + "不存在。");
+        }
+        location = program.getLabelMap().get(label);
+    }
+
+    @Override
+    public void reset(){
+        location = 0;
+    }
+
+    @Override
+    public boolean executeOne(){
+        if (location < program.getCommands().size()){
+            Command command = program.getCommands().get(location++);
+            if (command instanceof ExecutableCommand){
+                ((ExecutableCommand)command).execute(vm);
+            }
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public void executeAll(boolean fromStart){
+        if (fromStart){
+            reset();
+        }
+
+        while (executeOne()){
+            // Do nothing
+        }
+    }
+}
